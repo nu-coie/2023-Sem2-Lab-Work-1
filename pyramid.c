2aa7a45bda6d3b8137edf2050335f8eccb55fc8f
Saad Badie <107682899+saad-badie@users.noreply.github.com>
2023-04-10 01:13:19 +0300

    Create pyramid.c

---
+++ pyramid.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // Step ( 1 ) : Prompt the user for Size of Pyramid
+
+    
+
+    // Step ( 2 ) : Draw the Pyramid (Hint* : Use nested for loop)
+     
+}
