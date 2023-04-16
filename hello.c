831a70967da00ee01461ec952dd9434121ecc427
Saad Badie <107682899+saad-badie@users.noreply.github.com>
2023-04-16 20:17:28 +0000

    ddd, Apr 4/16/2023, YYYY, 8:17 A +0

--- hello.c
+++ hello.c
@@ -1,12 +1,24 @@
+// Buggy example for debug50
+
+#include <cs50.h>
 #include <stdio.h>
+
+int get_negative_int(void);
 
 int main(void)
 {
-  
-  // Step ( 1 ) : Print Hello, World
-  
-  
-  
-  //Step ( 2 ) : Print Hello, Your Name
+    int i = get_negative_int();
+    printf("%i\n", i);
+}
 
+// Prompt user for positive integer
+int get_negative_int(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Negative Integer: ");
+    }
+    while (n < 0);
+    return n;
 }
